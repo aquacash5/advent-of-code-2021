dbffc67daab2930eefc9a6cb0c2d740e05455791
Kyle Bloom <kylejbloom@gmail.com>
2021-12-12 12:54:56 +0000

    Use absolute difference

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -11,6 +11,20 @@ struct Cli {
     input: PathBuf,
 }
 
+trait AbsoluteDifference {
+    fn diff_abs(self, other: Self) -> Self;
+}
+
+impl AbsoluteDifference for u32 {
+    fn diff_abs(self, other: Self) -> Self {
+        if self < other {
+            other - self
+        } else {
+            self - other
+        }
+    }
+}
+
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -21,31 +35,29 @@ where
     Ok(io::BufReader::new(file).lines())
 }
 
-fn calc_fuel_cost_p1(xs: &Vec<i32>, pos: i32) -> i32 {
-    xs.iter().map(|i| (i - pos).abs()).sum()
+fn calc_fuel_cost_p1(xs: &Vec<u32>, pos: u32) -> u32 {
+    xs.iter().map(|i| i.diff_abs(pos)).sum()
 }
 
-fn calc_fuel_cost_p2(xs: &Vec<i32>, pos: i32) -> i32 {
-    xs.iter()
-        .map(|i| (1..((i - pos).abs() + 1)).sum::<i32>())
-        .sum()
+fn calc_fuel_cost_p2(xs: &Vec<u32>, pos: u32) -> u32 {
+    xs.iter().map(|i| (1..=i.diff_abs(pos)).sum::<u32>()).sum()
 }
 
 fn main() {
     let cli = Cli::from_args();
     if let Ok(lines) = read_lines(&cli.input) {
-        let data: Vec<i32> = lines
+        let data: Vec<u32> = lines
             .filter_map(Result::ok)
             .next()
             .unwrap()
             .split(",")
             .map(|s| s.parse().unwrap())
             .collect();
-        let part_1: i32 = (0..data.iter().max().unwrap().clone())
+        let part_1: u32 = (0..*data.iter().max().unwrap())
             .map(|i| calc_fuel_cost_p1(&data, i))
             .min()
             .unwrap();
-        let part_2: i32 = (0..data.iter().max().unwrap().clone())
+        let part_2: u32 = (0..*data.iter().max().unwrap())
             .map(|i| calc_fuel_cost_p2(&data, i))
             .min()
             .unwrap();
